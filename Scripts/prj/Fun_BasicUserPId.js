e7f7124ebbca47262289995ab30e981b50d3c008
brianlin12345 <brianlin12345@gamil.com>
2023-12-28 17:44:36 +0800

    PId v3

--- Scripts/prj/Fun_BasicUserPId.js
+++ Scripts/prj/Fun_BasicUserPId.js
@@ -6,13 +6,39 @@
         if (this.field == "PId") {
             this.formatter = function (v, d) {
                 //return v;
-                return '<div id="sumweek" weektype = "1" style="cursor: pointer; color: #1e0cfc;">' + v + '</div>';
+                return '<div id="divGo" style="cursor: pointer; color: #1e0cfc;">' + v + '</div>';
             }
             this.events = {
-                'click #sumweek': function (e, value, row, index) {
+                'click #divGo': function (e, value, row, index) {
                     $("#_table").bootstrapTable('showColumn', 'ctrl');
                     $(".btn-view-data-manager").eq(index).trigger("click");
                     $("#_table").bootstrapTable('hideColumn', 'ctrl');
+
+                    //append PId button
+                    setTimeout(function () {
+                        var footer = '<div class="modal-footer justify-content-start"> \
+                                          <div class="detail-view-field"> \
+                                             <b>新身分證</b> \
+                                          </div> \
+                                          <div class="field-content col-sm-3"> \
+                                             <input type="text" class="form-control" maxlength="20"> \
+                                          </div> \
+                                          <button id="btnUpdatePid" type="button" class="btn btn-primary " style=""> 確 定 </button> \
+                                      </div>';
+                        $('.modal-dialog .modal-body').append(footer);
+
+                        $('#btnUpdatePid').click(function () {
+                            jspConfirmYesNo($("body"), { content: "確認更新身分證資料" }, function (confrim) {
+                                if (confrim) {                                    
+                                    jspAlertMsg($("body"), { autoclose: 2000, content: '身分證更新成功!!', classes: 'modal-sm' },
+                                        function () {
+                                            location.reload();
+                                        });
+                                }
+                            })
+                        });
+                    }, 300);
+                    //End append PId button
                 }
             }
         }
