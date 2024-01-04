06dfefde04d8c9a30b8069cf1f08e43bc0226ed9
brianlin12345 <brianlin12345@gamil.com>
2024-01-04 10:07:14 +0800

    1

--- Controllers/Es/ExpertiseController.cs
+++ Controllers/Es/ExpertiseController.cs
@@ -60,7 +60,7 @@ namespace Esdms.Controllers.Es
 
             opts.GetFiled("PId").colsize = 6;
             opts.GetFiled("SubjectId").colsize = 6;
-            opts.GetFiled("SubjectDetailId").colsize = 6;
+            opts.GetFiled("SubjectDetailId").colsize = 12;
 
             opts.GetFiled("Note").colsize = 12;
             opts.GetFiled("Note").datatype = "textarea";
