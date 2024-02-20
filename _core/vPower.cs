90947ed018d25c190b6c1ce7cdd0d53c95cc2294
brianlin12345 <brianlin12345@gamil.com>
2024-02-20 11:56:15 +0800

    vpower v2

---
+++ _core/vPower.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Esdms
+{
+    public class vPower
+    {
+        public int serial { get; set; }
+        public string empno { get; set; }
+        public string vpower { get; set; }
+    }
+}
