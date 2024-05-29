ab15c5ea0ebc17efda2949d6fbe57d1378b99e72
brianlin12345 <brianlin12345@gamil.com>
2024-05-29 13:37:18 +0800

    加入Google行事曆 v1

---
+++ Controllers/Notify/GCalController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Esdms.Controllers.Notify
+{
+    [Dou.Misc.Attr.MenuDef(Id = "GCal", Name = "加入Google行事曆", Action = "Index", Index = -1, Func = Dou.Misc.Attr.FuncEnum.ALL, AllowAnonymous = true)]
+    public class GCalController : Controller
+    {
+        // GET: GCal
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
