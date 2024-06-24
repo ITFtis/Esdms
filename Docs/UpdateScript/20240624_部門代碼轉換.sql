1cb1401ea3e26ed06fb69625a54dc655a70793de
brianlin12345 <brianlin12345@gamil.com>
2024-06-24 10:35:12 +0800

    script

--- Docs/UpdateScript/20240624_部門代碼轉換.sql
+++ Docs/UpdateScript/20240624_部門代碼轉換.sql
@@ -1,10 +1,11 @@
 --部門代碼轉換
 
-Update FTISUserHistory Set DCode = '02' Where DCode = '1'
-Update FTISUserHistory Set DCode = '58' Where DCode = '2'
-Update FTISUserHistory Set DCode = '52' Where DCode = '3'
-Update FTISUserHistory Set DCode = '31' Where DCode = '4'
-Update FTISUserHistory Set DCode = '19' Where DCode = '5'
-Update FTISUserHistory Set DCode = '16' Where DCode = '6'
-Update FTISUserHistory Set DCode = '22' Where DCode = '7'
-Update FTISUserHistory Set DCode = '17' Where DCode = '8'
+Update FTISUserHistory Set DCode = '02' Where DCode = '1' And ActivityCategoryType = 1
+Update FTISUserHistory Set DCode = '58' Where DCode = '2' And ActivityCategoryType = 1
+Update FTISUserHistory Set DCode = '52' Where DCode = '3' And ActivityCategoryType = 1
+Update FTISUserHistory Set DCode = '31' Where DCode = '4' And ActivityCategoryType = 1
+Update FTISUserHistory Set DCode = '19' Where DCode = '5' And ActivityCategoryType = 1
+Update FTISUserHistory Set DCode = '16' Where DCode = '6' And ActivityCategoryType = 1
+Update FTISUserHistory Set DCode = '22' Where DCode = '7' And ActivityCategoryType = 1
+Update FTISUserHistory Set DCode = '17' Where DCode = '8' And ActivityCategoryType = 1
+
