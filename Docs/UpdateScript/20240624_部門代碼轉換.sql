b06a140da7ac55d647273a927760f8f7fa4dec0f
brianlin12345 <brianlin12345@gamil.com>
2024-06-24 10:23:33 +0800

    123

---
+++ Docs/UpdateScript/20240624_部門代碼轉換.sql
@@ -0,0 +1,10 @@
+--部門代碼轉換
+
+Update FTISUserHistory Set DCode = '02' Where DCode = '1'
+Update FTISUserHistory Set DCode = '58' Where DCode = '2'
+Update FTISUserHistory Set DCode = '52' Where DCode = '3'
+Update FTISUserHistory Set DCode = '31' Where DCode = '4'
+Update FTISUserHistory Set DCode = '19' Where DCode = '5'
+Update FTISUserHistory Set DCode = '16' Where DCode = '6'
+Update FTISUserHistory Set DCode = '22' Where DCode = '7'
+Update FTISUserHistory Set DCode = '17' Where DCode = '8'
