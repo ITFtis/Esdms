5e26e8c23a14db0dbd1e54125984ff24354f6d23
brianlin12345 <brianlin12345@gamil.com>
2024-07-05 15:13:00 +0800

    script

--- Docs/UpdateScript/20240705_會議不計評選次數.sql
+++ Docs/UpdateScript/20240705_會議不計評選次數.sql
@@ -1,12 +1,9 @@
 --會議不計評選次數(Y/N)
 --Select * From ActivityCategory
 
---alter Table ActivityCategory add IsNoCount [nvarchar](1) NULL
---Go
+alter Table ActivityCategory add IsCount [nvarchar](1) NULL
+Go
 
---Update ActivityCategory
---Set IsNoCount = 'N'
---Go
 
 Update ActivityCategory
 Set IsCount = 'Y'
