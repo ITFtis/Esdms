437d2172d34d745d18ac899869fe42ba5fe8ac22
brianlin12345 <brianlin12345@gamil.com>
2024-09-19 13:06:52 +0800

    doc 2

---
+++ Docs/需求/(專家資料暫存表)線上會議活動登錄-出席專家學者/線上系統更新欄位說明.sql
@@ -0,0 +1,13 @@
+Select *
+From z_DataMisBasicUser
+
+--(V)請線上系統更新委員資料
+--BasicName：委員姓名
+--(V)sno：序號(outMeet 線上會議活動)
+--(V)PrivatePhone：手機
+--(V)OfficeEmail：Email
+--BDate：建檔日
+--(V)UDate：更新日
+--(V)UMno：更新者職號
+--(V)UName：更新者姓名
+--(V)IsUpdate：是否異動(1有0無)
