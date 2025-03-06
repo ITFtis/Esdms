879401ea2d667e862c427723a0390d920f98f3c9
brianlin12345 <brianlin12345@gamil.com>
2025-03-06 15:24:14 +0800

    script

---
+++ Docs/UpdateScript/20250306_專家請款明細.sql
@@ -0,0 +1,11 @@
+--專家請款明細
+alter Table Project add [PrjID] [varchar](9) NULL
+alter Table Project add [PrjYear] [int] NULL
+alter Table Project add [OwnerA] [nvarchar](50) NULL
+alter Table Project add [OwnerB] [nvarchar](50) NULL
+alter Table Project add [BriefName] [nvarchar](20) NULL
+alter Table Project add [PrjStartDate] [date] NULL
+alter Table Project add [PrjEndDate] [date] NULL
+alter Table Project add [PjNoM] [varchar](20) NULL
+alter Table Project add [PjNameM] [nvarchar](100) NULL
+
