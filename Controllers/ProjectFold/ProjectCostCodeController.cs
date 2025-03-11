9c11d0f8a7105e47b4bc83ad8bbf3cb9a145c455
brianlin12345 <brianlin12345@gamil.com>
2025-03-11 14:04:05 +0800

    專案費用科目代碼

---
+++ Controllers/ProjectFold/ProjectCostCodeController.cs
@@ -0,0 +1,52 @@
+﻿using Dou.Models.DB;
+using Esdms.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Esdms.Controllers.ProjectFold
+{
+    [Dou.Misc.Attr.MenuDef(Id = "ProjectCostCode", Name = "專案費用科目代碼", MenuPath = "專案資料", Action = "Index", Index = 3, Func = Dou.Misc.Attr.FuncEnum.ALL, AllowAnonymous = false)]
+    public class ProjectCostCodeController : Dou.Controllers.AGenericModelController<ProjectCostCode>
+    {
+        // GET: ProjectCostCode
+        public ActionResult Index()
+        {
+            return View();
+        }
+        protected override Dou.Models.DB.IModelEntity<ProjectCostCode> GetModelEntity()
+        {
+            return new Dou.Models.DB.ModelEntity<ProjectCostCode>(new EsdmsModelContextExt());
+        }
+
+        protected override void AddDBObject(IModelEntity<ProjectCostCode> dbEntity, IEnumerable<ProjectCostCode> objs)
+        {
+            var f = objs.First();
+
+            f.BDate = DateTime.Now;
+            f.BFno = Dou.Context.CurrentUserBase.Id;
+
+            base.AddDBObject(dbEntity, objs);
+            ProjectCostCode.ResetGetAllDatas();
+        }
+
+        protected override void UpdateDBObject(IModelEntity<ProjectCostCode> dbEntity, IEnumerable<ProjectCostCode> objs)
+        {
+            var f = objs.First();
+
+            f.UDate = DateTime.Now;
+            f.UFno = Dou.Context.CurrentUserBase.Id;            
+
+            base.UpdateDBObject(dbEntity, objs);
+            ProjectCostCode.ResetGetAllDatas();
+        }
+
+        protected override void DeleteDBObject(IModelEntity<ProjectCostCode> dbEntity, IEnumerable<ProjectCostCode> objs)
+        {
+            base.DeleteDBObject(dbEntity, objs);
+            ProjectCostCode.ResetGetAllDatas();
+        }
+    }
+}
---
+++ Models/ProjectCostCode.cs
@@ -0,0 +1,75 @@
+﻿using Dou.Misc.Attr;
+using DouHelper;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace Esdms.Models
+{
+    /// <summary>
+    /// 專案費用科目代碼
+    /// </summary>
+    [Table("ProjectCostCode")]
+    public class ProjectCostCode
+    {
+        [Key]
+        [StringLength(20)]
+        [Display(Name = "代碼")]
+        public string Code { get; set; }
+
+        [StringLength(50)]
+        [Display(Name = "費用名稱")]
+        public string Name { get; set; }
+
+        [Display(Name = "排序")]
+        public int Sort { get; set; }
+
+        [Display(Name = "建檔日期")]
+        [ColumnDef(Visible = false, VisibleEdit = false, ColSize = 3)]
+        public DateTime? BDate { get; set; }
+
+        [Display(Name = "建檔人員編")]
+        [ColumnDef(Visible = false, VisibleEdit = false, ColSize = 3)]
+        [StringLength(6)]
+        public string BFno { get; set; }
+
+        [Display(Name = "修改日期")]
+        [ColumnDef(Visible = false, VisibleEdit = false, ColSize = 3)]
+        public DateTime? UDate { get; set; }
+
+        [Display(Name = "修改人員編")]
+        [ColumnDef(Visible = false, VisibleEdit = false, ColSize = 3)]
+        [StringLength(6)]
+        public string UFno { get; set; }
+
+        static object lockGetAllDatas = new object();
+        public static IEnumerable<ProjectCostCode> GetAllDatas(int cachetimer = 0)
+        {
+            if (cachetimer == 0) cachetimer = Constant.cacheTime;
+
+            string key = "Esdms.Models.ProjectCostCode";
+            var allData = DouHelper.Misc.GetCache<IEnumerable<ProjectCostCode>>(cachetimer, key);
+            lock (lockGetAllDatas)
+            {
+                if (allData == null)
+                {
+                    Dou.Models.DB.IModelEntity<ProjectCostCode> modle = new Dou.Models.DB.ModelEntity<ProjectCostCode>(new EsdmsModelContextExt());
+                    allData = modle.GetAll().ToArray();
+
+                    DouHelper.Misc.AddCache(allData, key);
+                }
+            }
+
+            return allData;
+        }
+
+        public static void ResetGetAllDatas()
+        {
+            string key = "Esdms.Models.ProjectCostCode";
+            Misc.ClearCache(key);
+        }
+    }
+}
